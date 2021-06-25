97a3cd6deede8ba582b1761565fa1b5e81687cb7
Alan Bielik <abielik92@gmail.com>
2021-06-25 09:34:17 -0400

    Age verification refactor

--- main.js
+++ main.js
@@ -5,51 +5,58 @@ const userBirthday = document.querySelector('#DOB');
 const ageErrorMessage = document.querySelector('#age-error-message');
 const ab = document.querySelector('#ab');
 
-confirmButton.addEventListener('click', checkAge);
+confirmButton.addEventListener('click', onConfirmButtonClick);
 
 // event listeners to transform "AB" on main page
 ab.addEventListener('mouseover', transformInitialsOnHover);
 ab.addEventListener('mouseout', resetInitialsAfterHover);
 
+// as soon as page loads, check localStorage
 checkLocalStorage();
 
-if (localStorage.checkbox) {
-  rememberMeCheckbox.setAttribute('checked', 'checked');
-} else {
-  rememberMeCheckbox.removeAttribute('checked');
-}
-
+// 2 functions for transforming "AB" during mouse hover and off
 function transformInitialsOnHover() {
   ab.innerText = "Alan's Brewery";
 }
-
 function resetInitialsAfterHover() {
   ab.innerText = 'AB';
 }
 
 function onConfirmButtonClick() {
-  ageGateContainer.style.display = 'none';
+  const age = getAge();
+  checkAge(age);
 }
-
-function checkAge() {
+/*
+  getAge() will first check number of years. If years equals 21, then need to compare the integer value of birth month vs currunt month. If birth month is same as the current month, then need to compare the birth day to see if that day has occured or not
+*/
+function getAge() {
   const userBirthdayValue = userBirthday.value;
   const DOB = new Date(userBirthdayValue);
   const today = new Date();
-  // Let user know if date is empty or invalid
-  if (!userBirthdayValue) {
+
+  const diffInYears = today.getFullYear() - DOB.getFullYear();
+  if (diffInYears !== 21) {
+    return diffInYears;
+  }
+  const diffInMonths = today.getMonth() - DOB.getMonth();
+  if (diffInMonths !== 0) {
+    return diffInYears;
+  }
+  const diffInDays = today.getDate() - DOB.getDate();
+  if (diffInDays >= 1) {
+    return diffInYears;
+  } else {
+    return diffInYears - 1;
+  }
+}
+
+function checkAge(age) {
+  if (!age) {
     ageErrorMessage.innerHTML = 'Please choose a date.';
     return false;
   }
-  let age = today.getFullYear() - DOB.getFullYear();
-  console.log(age);
-  const month = today.getMonth() - DOB.getMonth();
-  // ensure the month and day have passed otherwise subtract 1 from age
-  if (month < 0 || (month === 0 && today.getDate() < DOB.getDate())) {
-    age--;
-  }
-
   if (age >= 21) {
-    onConfirmButtonClick();
+    ageGateContainer.style.display = 'none';
     if (rememberMeCheckbox.checked) {
       setRememberMeStorage();
     }
@@ -60,12 +67,12 @@ function checkAge() {
 
 // checks if remember me box is checked upon age confirmation click
 function setRememberMeStorage() {
-  localStorage.checkbox = rememberMeCheckbox.value;
+  localStorage.is21 = true;
 }
 
 // first thing to run once page loads
 function checkLocalStorage() {
-  if (localStorage.checkbox === 'Remember Me') {
-    onConfirmButtonClick();
+  if (localStorage.is21) {
+    ageGateContainer.style.display = 'none';
   }
 }
