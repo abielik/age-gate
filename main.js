f0df24bc3c306e6d90226e56be7b91f2a7f9c32b
Alan Bielik <abielik92@gmail.com>
2021-06-25 14:04:27 -0400

    Bug: future month in birthday year fix

--- main.js
+++ main.js
@@ -23,26 +23,31 @@ function resetInitialsAfterHover() {
 }
 
 function onConfirmButtonClick() {
-  const age = getAge();
+  const age = getAge(new Date(userBirthday.value));
   checkAge(age);
 }
 /*
-  getAge() will first check number of years. If years equals 21, then need to compare the integer value of birth month vs currunt month. If birth month is same as the current month, then need to compare the birth day to see if that day has occured or not
+  getAge() will first check number of years. If years equals 21, then need to compare the integer value of the birth month vs the current month. If the birth month is the same as the current month, then need to compare the birth day to see if that day has occured or not
 */
-function getAge() {
-  const userBirthdayValue = userBirthday.value;
-  const DOB = new Date(userBirthdayValue);
+function getAge(birthday) {
   const today = new Date();
 
-  const diffInYears = today.getFullYear() - DOB.getFullYear();
+  const diffInYears = today.getFullYear() - birthday.getFullYear();
   if (diffInYears !== 21) {
     return diffInYears;
   }
-  const diffInMonths = today.getMonth() - DOB.getMonth();
-  if (diffInMonths !== 0) {
+  const diffInMonths = today.getMonth() - birthday.getMonth();
+  if (diffInMonths > 0) {
     return diffInYears;
   }
-  const diffInDays = today.getDate() - DOB.getDate();
+  if (diffInMonths < 0) {
+    return diffInYears - 1;
+  }
+  /*
+    if none of the above IF statements are truthy, this means the user is born in the current month, and the current year is the year they turn 21. Now it will determine if their birthday day has come or not
+  */
+  // the getDate() is slightly off which is why the If statement compares against the number 1 and not 0
+  const diffInDays = today.getDate() - birthday.getDate();
   if (diffInDays >= 1) {
     return diffInYears;
   } else {
@@ -51,9 +56,10 @@ function getAge() {
 }
 
 function checkAge(age) {
+  // must check age !== 0 for edge case: if birthday year equals the current year but the birthday month hasn't occured yet
   if (!age) {
-    ageErrorMessage.innerHTML = 'Please choose a date.';
-    return false;
+    ageErrorMessage.innerText = 'Please choose a date.';
+    return;
   }
   if (age >= 21) {
     ageGateContainer.style.display = 'none';
